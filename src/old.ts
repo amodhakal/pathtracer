3fca1176bd6c23876cf44a389f4a9855b1d4c032
Amodh Dhakal <amodhakal@gmail.com>
2025-10-11 00:43:48 -0400

    Added the old code

---
+++ src/old.ts
@@ -0,0 +1,624 @@
+import { vec3, vec4 } from "gl-matrix";
+
+// Interfaces and Types
+interface LightSource {
+  positionX: number;
+  positionY: number;
+  positionZ: number;
+  color: [number, number, number];
+}
+
+interface Material {
+  color: [number, number, number];
+}
+
+interface EllipsoidShape extends Material {
+  centerX: number;
+  centerY: number;
+  centerZ: number;
+  radiusX: number;
+  radiusY: number;
+  radiusZ: number;
+}
+
+type Point3D = [number, number, number];
+type Triangle3D = [Point3D, Point3D, Point3D];
+type Ray3D = [vec3, vec3];
+
+type IntersectionFound = {
+  exists: true;
+  intersectionPoint: vec3;
+  distance: number;
+};
+
+type IntersectionNotFound = {
+  exists: false;
+  intersectionPoint: typeof NaN;
+  distance: typeof NaN;
+};
+
+type Intersection = IntersectionFound | IntersectionNotFound;
+
+interface PixelCoordinates {
+  pixelX: number;
+  pixelY: number;
+  worldX: number;
+  worldY: number;
+}
+
+interface WallData {
+  triangles: Triangle3D[];
+  surfaceNormal: vec3;
+  material: Material;
+}
+
+interface HittableObject {
+  intersect(ray: Ray3D, minDistance: number): Intersection;
+  material: Material;
+  calculateNormal(point: vec3): vec3;
+}
+
+// HittableObject Classes
+class Ellipsoid implements HittableObject {
+  center: vec3;
+  radii: vec3;
+  radiiSquared: vec3;
+  material: Material;
+
+  constructor(ellipsoid: EllipsoidShape) {
+    this.center = vec3.fromValues(
+      ellipsoid.centerX,
+      ellipsoid.centerY,
+      ellipsoid.centerZ
+    );
+    this.radii = vec3.fromValues(
+      ellipsoid.radiusX,
+      ellipsoid.radiusY,
+      ellipsoid.radiusZ
+    );
+    this.radiiSquared = vec3.multiply(vec3.create(), this.radii, this.radii);
+    this.material = ellipsoid;
+  }
+
+  calculateNormal(point: vec3): vec3 {
+    const normalVector = vec3.subtract(vec3.create(), point, this.center);
+    vec3.divide(normalVector, normalVector, this.radiiSquared);
+    return vec3.normalize(normalVector, normalVector);
+  }
+
+  intersect(ray: Ray3D, minDistance: number): Intersection {
+    return calculateRayEllipsoidIntersection(ray, this, minDistance);
+  }
+}
+
+class Triangle implements HittableObject {
+  vertex0: vec3;
+  vertex1: vec3;
+  vertex2: vec3;
+  surfaceNormal: vec3;
+  material: Material;
+
+  constructor(triangle: Triangle3D, normal: vec3, material: Material) {
+    this.vertex0 = vec3.fromValues(
+      triangle[0][0],
+      triangle[0][1],
+      triangle[0][2]
+    );
+    this.vertex1 = vec3.fromValues(
+      triangle[1][0],
+      triangle[1][1],
+      triangle[1][2]
+    );
+    this.vertex2 = vec3.fromValues(
+      triangle[2][0],
+      triangle[2][1],
+      triangle[2][2]
+    );
+    this.surfaceNormal = normal;
+    this.material = material;
+  }
+
+  calculateNormal(_point: vec3): vec3 {
+    return this.surfaceNormal;
+  }
+
+  intersect(ray: Ray3D, minDistance: number): Intersection {
+    return calculateRayTriangleIntersection(ray, this, minDistance);
+  }
+}
+
+class Scene {
+  hittableObjects: HittableObject[] = [];
+
+  addObject(object: HittableObject): void {
+    this.hittableObjects.push(object);
+  }
+
+  findClosestIntersection(
+    ray: Ray3D,
+    minDistance: number
+  ): {
+    intersection: Intersection;
+    object: HittableObject | null;
+  } {
+    let closestDistance = Number.MAX_VALUE;
+    let closestIntersection: Intersection = {
+      exists: false,
+      intersectionPoint: NaN,
+      distance: NaN,
+    };
+    let closestObject: HittableObject | null = null;
+
+    for (const object of this.hittableObjects) {
+      const intersection = object.intersect(ray, minDistance);
+      if (intersection.exists && intersection.distance < closestDistance) {
+        closestDistance = intersection.distance;
+        closestIntersection = intersection;
+        closestObject = object;
+      }
+    }
+
+    return { intersection: closestIntersection, object: closestObject };
+  }
+}
+
+// Temp vectors to avoid allocations in critical loops
+const TEMP_VECTOR_1 = vec3.create();
+
+const LOCAL_DIRECTION_RS = vec3.create();
+const TANGENT_RS = vec3.create();
+const BINORMAL_RS = vec3.create();
+const UP_VECTOR_RS = vec3.create();
+const DOT_CHECK_VECTOR_RS = vec3.fromValues(0, 0, 1);
+
+const LIGHT_POSITION_DI = vec3.create();
+const TO_LIGHT_VECTOR_DI = vec3.create();
+const LIGHT_DIRECTION_DI = vec3.create();
+
+const BOUNCE_DIRECTION_II = vec3.create();
+const INDIRECT_RAY_COLOR_II = vec4.create();
+
+const DIRECT_LIGHT_PT = vec4.create();
+const INDIRECT_LIGHT_PT = vec4.create();
+
+const D_DIV_A_REI = vec3.create();
+const EM_C_REI = vec3.create();
+const EM_C_DIV_A_REI = vec3.create();
+
+const EDGE1_RTI = vec3.create();
+const EDGE2_RTI = vec3.create();
+const PVEC_RTI = vec3.create();
+const TVEC_RTI = vec3.create();
+const QVEC_RTI = vec3.create();
+
+// Constants
+const BOUNCE_PROBABILITY: number = 0.5;
+const WINDOW_Z: number = 0;
+const WINDOW_LEFT: number = 0,
+  WINDOW_RIGHT: number = 1;
+const WINDOW_BOTTOM: number = 0,
+  WINDOW_TOP: number = 1;
+
+// Scene Data
+const light: LightSource = {
+  positionX: 0.5,
+  positionY: 0.9999999999999,
+  positionZ: 0.5,
+  color: [1, 1, 1],
+};
+
+let Eye: vec3 = vec3.fromValues(0.5, 0.5, -0.5);
+
+const CORNELL_BOX_DATA: { [key: string]: WallData } = {
+  leftWall: {
+    triangles: [
+      [
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1],
+      ],
+      [
+        [0, 1, 0],
+        [0, 1, 1],
+        [0, 0, 1],
+      ],
+    ],
+    surfaceNormal: vec3.fromValues(1, 0, 0),
+    material: { color: [0.8, 0, 0] },
+  },
+  rightWall: {
+    triangles: [
+      [
+        [1, 0, 0],
+        [1, 0, 1],
+        [1, 1, 0],
+      ],
+      [
+        [1, 1, 0],
+        [1, 0, 1],
+        [1, 1, 1],
+      ],
+    ],
+    surfaceNormal: vec3.fromValues(-1, 0, 0),
+    material: { color: [0, 0, 0.8] },
+  },
+  backWall: {
+    triangles: [
+      [
+        [0, 0, 1],
+        [0, 1, 1],
+        [1, 0, 1],
+      ],
+      [
+        [1, 0, 1],
+        [0, 1, 1],
+        [1, 1, 1],
+      ],
+    ],
+    surfaceNormal: vec3.fromValues(0, 0, -1),
+    material: { color: [0.8, 0.8, 0.8] },
+  },
+  topWall: {
+    triangles: [
+      [
+        [0, 1, 0],
+        [1, 1, 0],
+        [0, 1, 1],
+      ],
+      [
+        [1, 1, 0],
+        [1, 1, 1],
+        [0, 1, 1],
+      ],
+    ],
+    surfaceNormal: vec3.fromValues(0, -1, 0),
+    material: { color: [0.8, 0.8, 0.8] },
+  },
+  bottomWall: {
+    triangles: [
+      [
+        [0, 0, 0],
+        [0, 0, 1],
+        [1, 0, 0],
+      ],
+      [
+        [1, 0, 0],
+        [0, 0, 1],
+        [1, 0, 1],
+      ],
+    ],
+    surfaceNormal: vec3.fromValues(0, 1, 0),
+    material: { color: [0.8, 0.8, 0.8] },
+  },
+};
+
+const makeYourOwnEllipsoids: EllipsoidShape[] = [
+  {
+    centerX: 0.5,
+    centerY: 0.5,
+    centerZ: 0.3,
+    radiusX: 0.15,
+    radiusY: 0.15,
+    radiusZ: 0.15,
+    color: [1.0, 0.8, 0.0],
+  },
+  {
+    centerX: 0.3,
+    centerY: 0.5,
+    centerZ: 0.25,
+    radiusX: 0.03,
+    radiusY: 0.03,
+    radiusZ: 0.03,
+    color: [0.7, 0.7, 0.7],
+  },
+];
+
+const scene = new Scene();
+makeYourOwnEllipsoids.forEach((e) => scene.addObject(new Ellipsoid(e)));
+Object.values(CORNELL_BOX_DATA).forEach((wall) => {
+  wall.triangles.forEach((t) =>
+    scene.addObject(new Triangle(t, wall.surfaceNormal, wall.material))
+  );
+});
+
+// Utility Functions
+function solveQuad(a: number, b: number, c: number): number[] {
+  const discr: number = b * b - 4 * a * c;
+  if (discr < 0) {
+    return [];
+  } else if (discr == 0) {
+    return [-b / (2 * a)];
+  } else {
+    const denom = 0.5 / a;
+    const term1 = -b;
+    const term2 = Math.sqrt(discr);
+    const tp = denom * (term1 + term2);
+    const tm = denom * (term1 - term2);
+    if (tm < tp) return [tm, tp];
+    else return [tp, tm];
+  }
+}
+
+function randPermutation(n: number): number[] {
+  const array: number[] = new Array(n);
+  let bagSize: number = n,
+    temp: number,
+    randChoice: number;
+  for (let i = 0; i < n; i++) array[i] = i;
+  while (bagSize !== 0) {
+    randChoice = Math.floor(Math.random() * bagSize);
+    bagSize--;
+    temp = array[bagSize];
+    array[bagSize] = array[randChoice];
+    array[randChoice] = temp;
+  }
+  return array;
+}
+
+function rejectionSampling(out: vec3, normal: vec3): void {
+  while (true) {
+    vec3.set(
+      LOCAL_DIRECTION_RS,
+      Math.random() * 2 - 1,
+      Math.random() * 2 - 1,
+      Math.random() * 2
+    );
+    if (
+      vec3.squaredLength(LOCAL_DIRECTION_RS) <= 1 &&
+      vec3.dot(DOT_CHECK_VECTOR_RS, LOCAL_DIRECTION_RS) >= 0
+    ) {
+      break;
+    }
+  }
+  vec3.normalize(LOCAL_DIRECTION_RS, LOCAL_DIRECTION_RS);
+  if (Math.abs(normal[0]) > 0.9) {
+    vec3.set(UP_VECTOR_RS, 0, 1, 0);
+  } else {
+    vec3.set(UP_VECTOR_RS, 1, 0, 0);
+  }
+  vec3.cross(TANGENT_RS, UP_VECTOR_RS, normal);
+  vec3.normalize(TANGENT_RS, TANGENT_RS);
+  vec3.cross(BINORMAL_RS, normal, TANGENT_RS);
+  vec3.scale(out, TANGENT_RS, LOCAL_DIRECTION_RS[0]);
+  vec3.scaleAndAdd(out, out, BINORMAL_RS, LOCAL_DIRECTION_RS[1]);
+  vec3.scaleAndAdd(out, out, normal, LOCAL_DIRECTION_RS[2]);
+}
+
+// Intersection Functions
+function calculateRayEllipsoidIntersection(
+  ray: Ray3D,
+  ellipsoid: Ellipsoid,
+  clipVal: number
+): Intersection {
+  vec3.divide(D_DIV_A_REI, ray[1], ellipsoid.radii);
+  const quadA = vec3.dot(D_DIV_A_REI, D_DIV_A_REI);
+  vec3.subtract(EM_C_REI, ray[0], ellipsoid.center);
+  vec3.divide(EM_C_DIV_A_REI, EM_C_REI, ellipsoid.radii);
+  const quadB = 2 * vec3.dot(D_DIV_A_REI, EM_C_DIV_A_REI);
+  const quadC = vec3.dot(EM_C_DIV_A_REI, EM_C_DIV_A_REI) - 1;
+  const qsolve = solveQuad(quadA, quadB, quadC);
+  for (let i = 0; i < qsolve.length; i++) {
+    const t = qsolve[i];
+    if (t >= clipVal) {
+      vec3.scaleAndAdd(TEMP_VECTOR_1, ray[0], ray[1], t);
+      return {
+        exists: true,
+        intersectionPoint: vec3.clone(TEMP_VECTOR_1),
+        distance: t,
+      };
+    }
+  }
+  return { exists: false, intersectionPoint: NaN, distance: NaN };
+}
+
+function calculateRayTriangleIntersection(
+  ray: Ray3D,
+  triangle: Triangle,
+  clipVal: number
+): Intersection {
+  vec3.subtract(EDGE1_RTI, triangle.vertex1, triangle.vertex0);
+  vec3.subtract(EDGE2_RTI, triangle.vertex2, triangle.vertex0);
+  const rayDir = ray[1];
+  vec3.cross(PVEC_RTI, rayDir, EDGE2_RTI);
+  const det = vec3.dot(EDGE1_RTI, PVEC_RTI);
+  if (Math.abs(det) < 1e-6) {
+    return { exists: false, intersectionPoint: NaN, distance: NaN };
+  }
+  const invDet = 1.0 / det;
+  vec3.subtract(TVEC_RTI, ray[0], triangle.vertex0);
+  const u = vec3.dot(TVEC_RTI, PVEC_RTI) * invDet;
+  if (u < 0 || u > 1) {
+    return { exists: false, intersectionPoint: NaN, distance: NaN };
+  }
+  vec3.cross(QVEC_RTI, TVEC_RTI, EDGE1_RTI);
+  const v = vec3.dot(rayDir, QVEC_RTI) * invDet;
+  if (v < 0 || u + v > 1) {
+    return { exists: false, intersectionPoint: NaN, distance: NaN };
+  }
+  const t = vec3.dot(EDGE2_RTI, QVEC_RTI) * invDet;
+  if (t < clipVal) {
+    return { exists: false, intersectionPoint: NaN, distance: NaN };
+  }
+  vec3.scaleAndAdd(TEMP_VECTOR_1, ray[0], ray[1], t);
+  return {
+    exists: true,
+    intersectionPoint: vec3.clone(TEMP_VECTOR_1),
+    distance: t,
+  };
+}
+
+// Path Tracing Logic
+function dirIllum(
+  out: vec4,
+  point: vec3,
+  normal: vec3,
+  material: Material,
+  light: LightSource,
+  scene: Scene
+): void {
+  vec3.set(
+    LIGHT_POSITION_DI,
+    light.positionX,
+    light.positionY,
+    light.positionZ
+  );
+  vec3.subtract(TO_LIGHT_VECTOR_DI, LIGHT_POSITION_DI, point);
+  const lightDist = vec3.length(TO_LIGHT_VECTOR_DI);
+  vec3.normalize(LIGHT_DIRECTION_DI, TO_LIGHT_VECTOR_DI);
+  let V = 1.0;
+  const { intersection: shadowIsect } = scene.findClosestIntersection(
+    [point, LIGHT_DIRECTION_DI],
+    0.001
+  );
+  if (shadowIsect.exists && shadowIsect.distance < lightDist) {
+    V = 0.0;
+  }
+  if (V === 0) {
+    vec4.set(out, 0, 0, 0, 255);
+    return;
+  }
+  const G =
+    (Math.max(0, vec3.dot(normal, LIGHT_DIRECTION_DI)) * 1.0) /
+    (1.0 + lightDist * lightDist);
+  const r = Math.min(255, 255 * light.color[0] * material.color[0] * G);
+  const g = Math.min(255, 255 * light.color[1] * material.color[1] * G);
+  const b = Math.min(255, 255 * light.color[2] * material.color[2] * G);
+  vec4.set(out, r, g, b, 255);
+}
+
+function indirIllum(
+  out: vec4,
+  point: vec3,
+  normal: vec3,
+  material: Material,
+  light: LightSource,
+  scene: Scene,
+  depth: number
+): void {
+  vec4.set(out, 0, 0, 0, 255);
+  if (depth > 1 && Math.random() > BOUNCE_PROBABILITY) {
+    return;
+  }
+  rejectionSampling(BOUNCE_DIRECTION_II, normal);
+  const bounceRay: Ray3D = [point, BOUNCE_DIRECTION_II];
+  pathTracer(INDIRECT_RAY_COLOR_II, bounceRay, light, scene, depth + 1);
+  const weight =
+    Math.max(0, vec3.dot(normal, BOUNCE_DIRECTION_II)) / BOUNCE_PROBABILITY;
+  out[0] += INDIRECT_RAY_COLOR_II[0] * material.color[0] * weight;
+  out[1] += INDIRECT_RAY_COLOR_II[1] * material.color[1] * weight;
+  out[2] += INDIRECT_RAY_COLOR_II[2] * material.color[2] * weight;
+}
+
+function pathTracer(
+  out: vec4,
+  ray: Ray3D,
+  light: LightSource,
+  scene: Scene,
+  depth: number
+): void {
+  const { intersection, object } = scene.findClosestIntersection(ray, 0.001);
+  if (!intersection.exists || !object) {
+    vec4.set(out, 0, 0, 0, 255);
+    return;
+  }
+  const normal = object.calculateNormal(intersection.intersectionPoint);
+  dirIllum(
+    DIRECT_LIGHT_PT,
+    intersection.intersectionPoint,
+    normal,
+    object.material,
+    light,
+    scene
+  );
+  indirIllum(
+    INDIRECT_LIGHT_PT,
+    intersection.intersectionPoint,
+    normal,
+    object.material,
+    light,
+    scene,
+    depth
+  );
+  const r = Math.min(255, DIRECT_LIGHT_PT[0] + INDIRECT_LIGHT_PT[0]);
+  const g = Math.min(255, DIRECT_LIGHT_PT[1] + INDIRECT_LIGHT_PT[1]);
+  const b = Math.min(255, DIRECT_LIGHT_PT[2] + INDIRECT_LIGHT_PT[2]);
+  vec4.set(out, r, g, b, 255);
+}
+
+// Rendering
+function getPixelLocat(
+  pixelNum: number,
+  w: number,
+  h: number
+): PixelCoordinates {
+  const y = Math.floor(pixelNum / w);
+  const x = pixelNum - y * w;
+  const wx = WINDOW_LEFT + (x / w) * (WINDOW_RIGHT - WINDOW_LEFT);
+  const wy = WINDOW_TOP + (y / h) * (WINDOW_BOTTOM - WINDOW_TOP);
+  return { pixelX: x, pixelY: y, worldX: wx, worldY: wy };
+}
+
+function pathTracingRayCast(context: CanvasRenderingContext2D): void {
+  const w = context.canvas.width;
+  const h = context.canvas.height;
+  const numPixels = w * h;
+  const imagedata = context.getImageData(0, 0, w, h);
+  const pixelColors = new Array(numPixels).fill(null).map(() => vec4.create());
+  const samples = new Array(numPixels).fill(0);
+  const MAX_SAMPLES = 256;
+  let currentSample = 0;
+  const pathColor = vec4.create();
+  const pixelWorldPos = vec3.create();
+  const rayDir = vec3.create();
+
+  function renderPass(): void {
+    if (currentSample >= MAX_SAMPLES) {
+      console.log("Rendering complete.");
+      return;
+    }
+    const pixelOrder = randPermutation(numPixels);
+    for (let i = 0; i < numPixels; i++) {
+      const pixelIndex = pixelOrder[i];
+      const pixelLocat = getPixelLocat(pixelIndex, w, h);
+      const wx = pixelLocat.worldX + (Math.random() - 0.5) / w;
+      const wy = pixelLocat.worldY + (Math.random() - 0.5) / h;
+      vec3.set(pixelWorldPos, wx, wy, WINDOW_Z);
+      vec3.subtract(rayDir, pixelWorldPos, Eye);
+      vec3.normalize(rayDir, rayDir);
+      pathTracer(pathColor, [Eye, rayDir], light, scene, 0);
+      vec4.add(pixelColors[pixelIndex], pixelColors[pixelIndex], pathColor);
+      samples[pixelIndex]++;
+      const numSamples = samples[pixelIndex];
+      const imgDataIndex = (pixelLocat.pixelY * w + pixelLocat.pixelX) * 4;
+      imagedata.data[imgDataIndex] = pixelColors[pixelIndex][0] / numSamples;
+      imagedata.data[imgDataIndex + 1] =
+        pixelColors[pixelIndex][1] / numSamples;
+      imagedata.data[imgDataIndex + 2] =
+        pixelColors[pixelIndex][2] / numSamples;
+    }
+    context.putImageData(imagedata, 0, 0);
+    currentSample++;
+    console.log(`Sample ${currentSample}/${MAX_SAMPLES}`);
+    requestAnimationFrame(renderPass);
+  }
+  for (let i = 3; i < imagedata.data.length; i += 4) {
+    imagedata.data[i] = 255;
+  }
+  requestAnimationFrame(renderPass);
+}
+
+function main(): void {
+  const canvas = document.querySelector("#canvas") as HTMLCanvasElement | null;
+  if (!canvas) {
+    console.error("Canvas element not found!");
+    return;
+  }
+  const context = canvas.getContext("2d");
+  if (!context) {
+    console.error("Could not get 2D context from canvas!");
+    return;
+  }
+  pathTracingRayCast(context);
+}
+
+window.onload = main;
